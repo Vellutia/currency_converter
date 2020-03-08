6ef45b84849778e6041a2dee957895cf180c1819
Vellutia <federickjonathan22@gmail.com>
2020-03-08 20:59:32 +0700

    Adding copy to clipboard & Visual onTap feedback

--- lib/ui/widget/home_page/bottom_value.dart
+++ lib/ui/widget/home_page/bottom_value.dart
@@ -1,6 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:currency_converter/bloc/data/dial_number_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/feature/currency_bloc.dart';
@@ -35,55 +35,96 @@ class BottomValue extends StatelessWidget {
                         ),
                         child: Text(
                           '${curState.id}',
-                          style: Theme.of(context).accentTextTheme.headline5,
+                          style: Theme.of(context)
+                              .accentTextTheme
+                              .headline5
+                              .copyWith(
+                                color: Theme.of(context)
+                                    .accentTextTheme
+                                    .headline5
+                                    .color
+                                    .withOpacity(0.6),
+                              ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () => show(context, constraints, false),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Flexible(
-                              fit: FlexFit.loose,
-                              child: AutoSizeText(
-                                curState.value.toString().startsWith('0.')
-                                    ? curState.value.toString().length < 4
-                                        ? '${curState.value.toString().substring(0, 3)}'
-                                        : '${curState.value.toString().substring(0, 4)}'
-                                    : curState.value
-                                            .toStringAsFixed(1)
-                                            .contains('.0')
-                                        ? '${curState.value.toStringAsFixed(1)}'
-                                        : '${curState.value.toStringAsFixed(2)}',
-                                maxLines: 1,
-                                minFontSize: 12.0,
-                                style:
-                                    Theme.of(context).accentTextTheme.headline1,
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(18.0),
+                          onTap: () => show(context, constraints, true),
+                          onLongPress: () => Clipboard.setData(
+                            ClipboardData(text: '${curState.value}'),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Tooltip(
+                              message: 'Copied to clipboard.',
+                              preferBelow: true,
+                              verticalOffset: constraints.maxHeight * 0.15,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Flexible(
+                                    fit: FlexFit.loose,
+                                    child: AutoSizeText(
+                                      curState.value.toString().startsWith('0.')
+                                          ? curState.value.toString().length < 4
+                                              ? '${curState.value.toString().substring(0, 3)}'
+                                              : '${curState.value.toString().substring(0, 4)}'
+                                          : curState.value
+                                                  .toStringAsFixed(1)
+                                                  .contains('.0')
+                                              ? '${curState.value.toStringAsFixed(1)}'
+                                              : '${curState.value.toStringAsFixed(2)}',
+                                      maxLines: 1,
+                                      minFontSize: 12.0,
+                                      style: Theme.of(context)
+                                          .accentTextTheme
+                                          .headline1,
+                                    ),
+                                  ),
+                                  horizontalSpaceTiny,
+                                  Text(
+                                    '${curState.symbol}',
+                                    style: Theme.of(context)
+                                        .accentTextTheme
+                                        .headline5
+                                        .copyWith(
+                                          color: Theme.of(context)
+                                              .accentTextTheme
+                                              .headline5
+                                              .color
+                                              .withOpacity(0.6),
+                                        ),
+                                  ),
+                                ],
                               ),
                             ),
-                            horizontalSpaceTiny,
-                            Text(
-                              '${curState.symbol}',
-                              style:
-                                  Theme.of(context).accentTextTheme.headline5,
-                            ),
-                          ],
+                          ),
                         ),
                       ),
                       Padding(
                         padding: EdgeInsets.only(
                           bottom: constraints.maxHeight * 0.2,
                         ),
-                        child: GestureDetector(
-                          onTap: () => Router.navigator.pushNamed(
-                            Router.currencyPage,
-                            arguments: CurrencyPageArguments(isTop: false),
-                          ),
-                          child: Text(
-                            '${curState.name}',
-                            style: Theme.of(context).accentTextTheme.headline4,
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(18.0),
+                            onTap: () => Router.navigator.pushNamed(
+                              Router.currencyPage,
+                              arguments: CurrencyPageArguments(isTop: false),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '${curState.name}',
+                                style:
+                                    Theme.of(context).accentTextTheme.headline4,
+                              ),
+                            ),
                           ),
                         ),
                       ),
--- lib/ui/widget/home_page/top_value.dart
+++ lib/ui/widget/home_page/top_value.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/feature/currency_bloc.dart';
@@ -32,50 +33,82 @@ class TopValue extends StatelessWidget {
                         padding: EdgeInsets.only(
                           top: constraints.maxHeight * 0.2,
                         ),
-                        child: GestureDetector(
-                          onTap: () => Router.navigator.pushNamed(
-                            Router.currencyPage,
-                            arguments: CurrencyPageArguments(isTop: true),
-                          ),
-                          child: Text(
-                            '${curState.name}',
-                            style: Theme.of(context).primaryTextTheme.headline4,
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(18.0),
+                            onTap: () => Router.navigator.pushNamed(
+                              Router.currencyPage,
+                              arguments: CurrencyPageArguments(isTop: true),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '${curState.name}',
+                                style: Theme.of(context)
+                                    .primaryTextTheme
+                                    .headline4,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () => show(context, constraints, true),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Flexible(
-                              fit: FlexFit.loose,
-                              child: AutoSizeText(
-                                curState.value.toString().startsWith('0.')
-                                    ? curState.value.toString().length < 4
-                                        ? '${curState.value.toString().substring(0, 3)}'
-                                        : '${curState.value.toString().substring(0, 4)}'
-                                    : curState.value
-                                            .toStringAsFixed(1)
-                                            .contains('.0')
-                                        ? '${curState.value.toStringAsFixed(1)}'
-                                        : '${curState.value.toStringAsFixed(2)}',
-                                maxLines: 1,
-                                minFontSize: 12.0,
-                                style: Theme.of(context)
-                                    .primaryTextTheme
-                                    .headline1,
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(18.0),
+                          onTap: () => show(context, constraints, true),
+                          onLongPress: () => Clipboard.setData(
+                            ClipboardData(text: '${curState.value}'),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Tooltip(
+                              message: 'Copied to clipboard.',
+                              preferBelow: false,
+                              verticalOffset: constraints.maxHeight * 0.15,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Flexible(
+                                    fit: FlexFit.loose,
+                                    child: AutoSizeText(
+                                      curState.value.toString().startsWith('0.')
+                                          ? curState.value.toString().length < 4
+                                              ? '${curState.value.toString().substring(0, 3)}'
+                                              : '${curState.value.toString().substring(0, 4)}'
+                                          : curState.value
+                                                  .toStringAsFixed(1)
+                                                  .contains('.0')
+                                              ? '${curState.value.toStringAsFixed(1)}'
+                                              : '${curState.value.toStringAsFixed(2)}',
+                                      maxLines: 1,
+                                      minFontSize: 12.0,
+                                      style: Theme.of(context)
+                                          .primaryTextTheme
+                                          .headline1,
+                                    ),
+                                  ),
+                                  horizontalSpaceTiny,
+                                  Text(
+                                    '${curState.symbol}',
+                                    style: Theme.of(context)
+                                        .primaryTextTheme
+                                        .headline5
+                                        .copyWith(
+                                          color: Theme.of(context)
+                                              .primaryTextTheme
+                                              .headline5
+                                              .color
+                                              .withOpacity(0.6),
+                                        ),
+                                  ),
+                                ],
                               ),
                             ),
-                            horizontalSpaceTiny,
-                            Text(
-                              '${curState.symbol}',
-                              style:
-                                  Theme.of(context).primaryTextTheme.headline5,
-                            ),
-                          ],
+                          ),
                         ),
                       ),
                       Padding(
@@ -84,7 +117,16 @@ class TopValue extends StatelessWidget {
                         ),
                         child: Text(
                           '${curState.id}',
-                          style: Theme.of(context).primaryTextTheme.headline5,
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .headline5
+                              .copyWith(
+                                color: Theme.of(context)
+                                    .primaryTextTheme
+                                    .headline5
+                                    .color
+                                    .withOpacity(0.6),
+                              ),
                         ),
                       ),
                     ],
