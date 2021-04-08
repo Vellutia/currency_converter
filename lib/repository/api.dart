80ae19f80da58e3491186c78adb4508b8cba2989
Federick Jonathan <federickjonathan22@gmail.com>
2021-04-08 17:14:26 +0700

    change to another endpoint : https://api.exchangerate.host'

--- lib/repository/api.dart
+++ lib/repository/api.dart
@@ -6,7 +6,7 @@ import '../model/rate.dart';
 import '../model/rate_crypto.dart';
 
 class Api {
-  static const baseEndpoint = 'https://openexchangerates.org/api';
+  static const baseEndpoint = 'https://api.exchangerate.host';
   // static const baseEndpoint = 'https://api.exchangeratesapi.io';
   // static const header = {
   //   HttpHeaders.contentTypeHeader: "application/json",
@@ -17,16 +17,13 @@ class Api {
     'Accept': "application/json",
     'Accept-Encoding': "application/gzip"
   };
-  // Get your own APP ID from openexchangerates - https://openexchangerates.org/
-  static const appID = '6fef2514d2b04f53adb5d95db2e6b753';
   // Get your own API key from CoinAPI - https://www.coinapi.io/
   static const apiKey = 'F792B90B-1CF3-44FA-AA5C-F30B080C30D9';
   var client = new http.Client();
 
   Future<double> fetchRates(String base, String symbol) async {
     var response = await client.get(
-      Uri.encodeFull(
-          '$baseEndpoint/latest.json?app_id=$appID&base=$base&symbols=$symbol'),
+      Uri.encodeFull('$baseEndpoint/latest?base=$base&symbols=$symbol'),
       // Uri.encodeFull('$baseEndpoint/latest?base=$base&symbols=$symbol'),
       // headers: header,
     );
